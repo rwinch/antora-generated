ac3e907adebe056c3aa3c241c84fee168df1a4b0
Rob Winch <rwinch@users.noreply.github.com>
2021-10-25 23:25:46 -0500

    Different generated

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -6,12 +6,12 @@ mkdir -p $OUT_DIR
 # Generate antora.yml which contains a dynamic version and asciidoc attributes
 echo "title: Component with name not in URL
 name: generated
-version: '2.0'
+version: '1'
 start_page: ROOT:index.adoc
 asciidoc:
   attributes:
-    spring_boot_version: 2.3.0.RELEASE
-    hibernate_version: 5
+    spring_boot_version: 1.2.0.RELEASE
+    hibernate_version: 4
 " > "$OUT_DIR/antora.yml"
 
 GENERATED_FILE_PATH="$OUT_DIR/modules/ROOT/pages/generated.adoc"
